18efc048000053efa2f1398d0a8b766515f4ee27
SatyamThorat <112162630+SatyamThorat@users.noreply.github.com>
2022-08-26 14:09:14 +0530

    Add files via upload

---
+++ integerarray1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SatyamPro.Arraypro
+{
+    internal class integerarray1
+    {
+        static void Main(string[] args)
+        {
+            //array reverse print
+            int[] a = new int[5];
+            a[0] = 5;
+            a[1] = 6;
+            a[3] = 7;
+            a[2] = 8;
+            a[4] = 9;
+            for (int i = a.Length - 1; i >= 0; i--)
+            {
+                Console.WriteLine(a[i]+"  ");
+
+            }
+        }
+
+    }
+    class arrayeven
+    {   //display even elemeny from array 
+        static void Main(string[] args)
+        {
+            int[] a = { 4, 5, 6, 7, 8, 9, 10 };
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (a[i] % 2 ==0)
+                {
+                    Console.WriteLine(a[i]);
+                }
+            }
+        }
+    }
+    class arrayevensum
+    {   //sum of even element 
+        static void Main(string[] args)
+        {
+            int[] a = { 4, 5, 6, 7, 8, 9, 10 };
+            int sum = 0;
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (a[i] % 2 == 0)
+                {
+                    sum = sum + a[i];
+                }
+            }
+            Console.WriteLine("sum of even element  = "+sum);
+        }
+    }
+    class sumarray
+    {
+        static void Main(string[] args)
+        {
+            int[] a = { 4, 5, 6, 7, 8, 9, 10 };
+            int sum = 0;
+            for (int i = 0; i < a.Length; i++)
+            {
+                
+                    sum = sum + a[i];
+                
+            }
+            Console.WriteLine("sum of araay element  = " + sum);
+        }
+    }
+    class oddposi1
+    {
+        static void Main(string[] args)
+        {
+            int[] a = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            for(int i = 0; i < a.Length; i++)
+            {
+                Console.WriteLine(i+"  "+a[i]);
+            }
+        }
+    }
+    
+
+    class oddposi
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter any five elements....");
+            int[] a = new int[5];
+            a[0] = int.Parse(Console.ReadLine());
+            a[1] = int.Parse(Console.ReadLine());
+            a[2] = int.Parse(Console.ReadLine());
+            a[3] = int.Parse(Console.ReadLine());
+            a[4] = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (a[i] % 2 == 1)
+                {
+                    Console.WriteLine(i+"  "+a[i]);
+                }
+            }
+        }
+    }
+}
---
+++ study2D21.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SatyamPro.Arraypro
+{
+    internal class study2D21
+    {
+        //to calculate sum of row in 2D array
+        static void Main(string[] args)
+        {
+            int[,] ar = { { 1, 2, 3 }, { 6, 7, 8 }, { 9, 5, 4 } };
+
+            for (int i = 0; i < ar.GetLength(0); i++)
+            {
+                int sum = 0;
+                for (int j = 0; j < ar.GetLength(1); j++)
+                {
+                    sum = sum + ar[i, j];
+                    Console.Write(ar[i, j] + " ");
+                }
+                Console.Write(" sum=" + sum);
+                Console.WriteLine();
+            }
+        }
+    }
+    class fourbyfour
+    {
+            //4*4
+            static void Main(string[] args)
+            {
+                int i, j;
+                int[,] ary = { { 1, 2, 3, 4 }, { 8, 7, 6, 5 }, { 1, 8, 2, 7 }, { 3, 6, 4, 5 } };
+                for (i = 0; i < ary.GetUpperBound(0); i++)
+                {
+                    for (j = 0; j < ary.GetUpperBound(1); j++)
+                    {
+                        Console.Write(ary[i, j] + " ");
+                    }
+                    Console.WriteLine();
+
+                    if (i == 0 || j == 0 || i == ary.GetUpperBound(0) || j == ary.GetUpperBound(1))
+                    {
+                        Console.Write(ary[i, j]);
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                    Console.WriteLine();
+                }
+            }
+    }
+    class p1
+    {
+        //patturn
+        static void Main(string[] args)
+        {
+            char[,] arr = new char[3, 3];
+            for (int i = 0; i < arr.GetUpperBound(0); i++)
+            {
+                for (int j = 0; j < arr.GetUpperBound(1); j++)
+                {
+                    if (i == 0 || j == 0 || i == arr.GetUpperBound(0) || j == arr.GetUpperBound(1))
+                    {
+                        arr[i, j] = '*';
+                    }
+                    else
+                    {
+                        arr[i, j] = ' ';
+                    }
+                    for (i = 0; i < arr.GetUpperBound(0); i++)
+                    {
+                        for (j = 0; j < arr.GetUpperBound(1); j++)
+                        {
+                            Console.Write(arr[i, j]);
+                        }
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+     class avr2D
+    {
+        //find out avarage of each row 
+        static void Main(string[] args)
+        {
+            int[,] ar = { { 1, 2, 3 }, { 6, 7, 8 }, { 9, 5, 4 } };
+            int i,j, avr;
+
+            for ( i = 0; i < ar.GetLength(0); i++)
+            {
+                int sum = 0;
+                for ( j = 0; j < ar.GetLength(1); j++)
+                {
+                    sum = sum + ar[i, j];
+                    Console.Write(ar[i, j] + " ");
+                }
+                avr = sum/(i * j);
+                Console.Write(" avarage="+avr);
+                Console.WriteLine();
+            }
+        }
+    }
+    class colsum
+    {
+        //coloum sum
+        static void Main(string[] args)
+        {
+            int[,] ar = { { 1, 2, 3 }, { 6, 7, 8 }, { 9, 5, 4 } };
+            for (int i = 0; i < ar.GetLength(1); i++)
+            {
+                int sum = 0;
+                for (int j = 0; j < ar.GetLength(0); j++)
+                {
+                    sum = sum + ar[i, j];
+                    Console.Write(ar[i, j] + " ");
+                }
+                Console.Write(" sum=" + sum);
+                Console.WriteLine();
+            }
+        }
+    }
+
+    class add2matrix
+    {
+        //adition of two matrices
+        static void Main(string[] args)
+        {
+            int[,] ar = { { 1, 2, 3 }, { 6, 7, 8 }, { 9, 5, 4 } };
+            int[,] br = { { 1, 2, 3 }, { 6, 7, 8 }, { 9, 5, 4 } };
+            int r = ar.GetLength(0), c = ar.GetLength(1);
+            int[,] sum = new int[r, c];
+            for(int i = 0; i < r; i++)
+            {
+                for(int j = 0; j < c; j++)
+                {
+                    sum[i, j] = ar[i, j] + br[i, j];
+                }
+            }
+            Console.WriteLine("addition of 2 matrices");
+            for (int i = 0; i < r; i++)
+            {
+                for (int j = 0; j < c; j++)
+                {
+                    Console.Write(sum[i,j]+" ");
+                }
+                Console.WriteLine();
+            }
+           
+        }
+    }
+    class patturnN
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
